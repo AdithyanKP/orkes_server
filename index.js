b07d0ff63c4abbbe2a42e1ac022197d2940069d8
Adithyan k P <adithyankp777@gmail.com>
2023-08-15 11:11:01 +0530

    fetch route added

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import express from "express";
+import axios from "axios";
 const app = express();
 const port = 3000;
 
+let baseUrl =
+  "https://englishapi.pinkvilla.com/app-api/v1/photo-gallery-feed-page/page";
+
 // Define a route
-app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+app.get("/orkes", async (req, res) => {
+  let page = req.query.page;
+  try {
+    const response = await axios.get(`${baseUrl}/${page}`);
+    const data = response?.data?.nodes;
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // Start the server
