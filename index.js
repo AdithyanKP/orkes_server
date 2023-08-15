126f1be271f96f43fad4702c142268dca89426ef
Adithyan k P <adithyankp777@gmail.com>
2023-08-15 11:56:15 +0530

    changes added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import axios from "axios";
+import cors from "cors";
 const app = express();
 const port = 3000;
 
 let baseUrl =
   "https://englishapi.pinkvilla.com/app-api/v1/photo-gallery-feed-page/page";
+
+// Use the cors middleware
+app.use(cors());
 
 // Define a route
 app.get("/orkes", async (req, res) => {
@@ -12,8 +16,13 @@ app.get("/orkes", async (req, res) => {
   try {
     const response = await axios.get(`${baseUrl}/${page}`);
     const data = response?.data?.nodes;
-    res.send(data);
+    if (data) {
+      res.send(data);
+    } else {
+      res.send([]);
+    }
   } catch (error) {
+    res.send([]);
     console.log(error);
   }
 });
